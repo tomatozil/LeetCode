11aa239424ba295dd6faee8294e77ee726978d3c
tomatozil <70049994+tomatozil@users.noreply.github.com>
2025-03-27 02:09:44 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -4,20 +4,16 @@ class Solution:
 
         nums.sort()
         for i in range(len(nums)):
-            if i > 0 and nums[i] == nums[i-1]:
-                continue
             target = -nums[i]
             l, r = i+1, len(nums)-1
             
             while l < r:
                 if nums[l] + nums[r] == target:
-                    triplets.append(sorted([nums[l], nums[r], nums[i]]))
+                    new_list = sorted([nums[l], nums[r], nums[i]])
+                    if new_list not in triplets:
+                        triplets.append(new_list)
                     l += 1
                     r -= 1
-                    while l < r and nums[l] == nums[l-1]:
-                        l += 1
-                    while l < r and nums[r] == nums[r+1]:
-                        r -= 1 
                 elif nums[l] + nums[r] < target:
                     l += 1
                 else:
