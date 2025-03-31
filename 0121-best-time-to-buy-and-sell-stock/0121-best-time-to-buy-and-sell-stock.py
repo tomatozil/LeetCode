1dbc37e0cc5533f7689c415d4b077c226870c6a2
tomatozil <70049994+tomatozil@users.noreply.github.com>
2025-04-01 00:28:41 +0900

    Time: 119 ms (37.11%), Space: 26.9 MB (33.03%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,21 +1,16 @@
-import heapq
-
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        max_heap = [(-price, idx) for idx, price in enumerate(prices)]
-        heapq.heapify(max_heap)
-
-        min_price = float('inf')
+        buy = 0
+        sell = 1
         max_profit = 0
 
-        while max_heap:
-            neg_price, sell_idx = heapq.heappop(max_heap)
-            sell_price = -neg_price
-            
-            for i in range(sell_idx):
-                min_price = min(min_price, prices[i])
-            
-            profit = sell_price - min_price
-            max_profit = max(max_profit, profit)
+        while sell < len(prices):
+            profit = prices[sell] - prices[buy]
 
+            if profit < 0:
+                buy = sell
+            else:
+                max_profit = max(max_profit, profit)
+            sell += 1
+        
         return max_profit
