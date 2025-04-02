29d6954d370291be88fb0119bdd8725d4f0e30ae
tomatozil <70049994+tomatozil@users.noreply.github.com>
2025-04-02 21:51:16 +0900

    Time: 3 ms (21.9%), Space: 18 MB (32.16%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,15 +1,21 @@
+from collections import deque
+
 class Solution:
     def isValid(self, s: str) -> bool:
+        s_q = deque(s)
         brackets = {'(': ')', '{': '}', '[': ']'}
         stack = []
         
-        for c in s:
-            if c in list(brackets):
-                stack.append(c)
+        while s_q:
+            b = s_q.popleft()
+            if b in list(brackets):
+                stack.append(b)
             else:
-                if stack and brackets[stack[-1]] == c:
-                    stack.pop()
-                else:
+                if not stack: return False
+                open_bracket = stack.pop()
+                if b != brackets[open_bracket]:
                     return False
         
-        return not stack
+        if stack:
+            return False
+        return True
