845c06a79e089f23d002a1ff5cd7acaec9cbba62
tomatozil <70049994+tomatozil@users.noreply.github.com>
2025-04-16 14:32:28 +0900

    Time: 42 ms (93.34%), Space: 113.1 MB (68.54%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.java
+++ 0146-lru-cache/0146-lru-cache.java
@@ -1,30 +1,36 @@
 class Node {
     int key;
     int value;
+    Node prev;
+    Node next;
 
     Node(int key, int value) {
         this.key = key;
         this.value = value;
+        this.prev = null;
+        this.next = null;
     }
 }
 
 class LRUCache {
     private Map<Integer, Node> cache;
-    private LinkedList<Node> memory;
+    private Node head = new Node(-1, -1);
+    private Node tail = new Node(-1, -1);
     private int capacity;
 
     public LRUCache(int capacity) {
         this.cache = new HashMap<>();
-        this.memory = new LinkedList<>();
+        this.head.next = this.tail;
+        this.tail.prev = this.head;
         this.capacity = capacity;
     }
     
     public int get(int key) {
         if (cache.containsKey(key)) {
-            Node targetNode = cache.get(key);
-            memory.remove(targetNode);
-            memory.offerLast(targetNode);
-            return targetNode.value;
+            Node target = cache.get(key);
+            this.remove(target);
+            this.addLast(target);
+            return target.value;
         }
         return -1;
     }
@@ -36,19 +42,35 @@ class LRUCache {
         // 2. node를 linked list에서 삭제하고 add last 한다
         // capacity와 cache 사이즈를 비교해서 초과시 remove first 한다
         if (cache.containsKey(key)) {
-            Node targetNode = cache.get(key);
-            memory.remove(targetNode);
-            memory.offerLast(targetNode);
-            targetNode.value = value;
+            Node target = cache.get(key);
+            this.remove(target);
+            this.addLast(target);
+            target.value = value;
         } else {
-            Node newNode = new Node(key, value);
-            memory.offer(newNode);
-            cache.put(key, newNode);
+            Node newbe = new Node(key, value);
+            this.addLast(newbe);
+            cache.put(key, newbe);
         }
 
         if (capacity < cache.size()) {
-            Node targetNode = memory.pollFirst();
-            cache.remove(targetNode.key);
+            Node target = this.head.next;
+            this.remove(target);
+            cache.remove(target.key);
         }
+    }
+
+    public void addLast(Node target) {
+        Node last = this.tail.prev;
+        last.next = target;
+        target.prev = last;
+        this.tail.prev = target;
+        target.next = this.tail;
+    }
+
+    public void remove(Node target) {
+        Node prev = target.prev;
+        Node next = target.next;
+        prev.next = next;
+        next.prev = prev;
     }
 }
