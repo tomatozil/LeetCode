dfccc0f649bbd57b9680ae8fdcd7be133be151d6
tomatozil <70049994+tomatozil@users.noreply.github.com>
2025-04-29 14:29:21 +0900

    Time: 9 ms (8.59%), Space: 9.6 MB (74.42%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,13 +1,9 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        string s_copy = s;
-        string t_copy = t;
-        sort(s_copy.begin(), s_copy.end());
-        sort(t_copy.begin(), t_copy.end());
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
 
-        if (s_copy == t_copy)
-            return true;
-        return false;
+        return s == t;
     }
 };
