3759f964f52c9177da2be818d38a8a2838a81e0f
tomatozil <tomatoziiilll@gmail.com>
2025-05-05 12:28:58 +0900

    Time: 0 ms (100%), Space: 17.6 MB (85.18%) - LeetHub

--- 0394-decode-string/0394-decode-string.py
+++ 0394-decode-string/0394-decode-string.py
@@ -2,24 +2,47 @@
 # 2[4[ab]c]
 class Solution:
     def decodeString(self, s: str) -> str:
-        stack = []
-        for i in range(len(s)):
-            if not s[i] == "]":
-                stack.append(s[i])
-            else:
-                sub = ""
-                while stack and stack[-1] != "[":
-                    top = stack.pop()
-                    sub = top + sub
-                stack.pop() # "["" 빼기
-
-                digit = ""
-                while stack and stack[-1].isdigit():
-                    top = stack.pop()
-                    digit = top + digit
-                
-                re = int(digit) # 숫자 추출
-                for r in range(re):
-                    stack.append(sub)
+        def decode(i) -> (str, int):
+            ret = ""
+            while i < len(s):
+                if s[i] == "]":
+                    return ret, i+1
+                else:
+                    if not s[i].isdigit():
+                        ret += s[i]
+                        i += 1
+                    else:
+                        digit = ""
+                        while i < len(s) and s[i].isdigit():
+                            digit += s[i]
+                            i += 1
+                        sub, jump = decode(i+1) # [ 다음부터 줌
+                        re = int(digit) # 숫자 추출
+                        for r in range(re):
+                            ret += sub
+                        i = jump
+            return ret, i       
         
-        return ''.join(stack)
+        return decode(0)[0]
+        
+        # stack = []
+        # for i in range(len(s)):
+        #     if not s[i] == "]":
+        #         stack.append(s[i])
+        #     else:
+        #         sub = ""
+        #         while stack and stack[-1] != "[":
+        #             top = stack.pop()
+        #             sub = top + sub
+        #         stack.pop() # "["" 빼기
+
+        #         digit = ""
+        #         while stack and stack[-1].isdigit():
+        #             top = stack.pop()
+        #             digit = top + digit
+                
+        #         re = int(digit) # 숫자 추출
+        #         for r in range(re):
+        #             stack.append(sub)
+        
+        # return ''.join(stack)
